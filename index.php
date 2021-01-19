cbb921339c8bb978c4104ac4d6a2bdad96d6c1f1
HF <hannibal.foks@mail.ru>
2021-01-19 18:35:13 +0300

    Первая часть создания страницы сайта компании перевозок по шаблону Figma.

---
+++ index.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;700&display=swap"
+          rel="stylesheet">
+
+    <title>Document</title>
+</head>
+<body>
+<main class="main-box">
+    <section class="header flex-group_column">
+        <div class="header__item1">
+            <div class="header__item1__elems flex-group_sp-btvn">
+                <div class="header__item1__lang flex-group_sp-btvn">
+                    <div class="header__item1__lang__elem header__elem">
+                        <a href="#">РУС</a>
+                    </div>
+                    <div class="header__item1__lang__elem header__elem">
+                        <a href="#">ENG</a>
+                    </div>
+                    <div class="header__item1__lang__elem header__elem">
+                        <a href="#">FR</a>
+                    </div>
+                </div>
+                <div class="header__item1__phone header__elem">
+                    <a href="#" class="header__item1__phone__a">
+                        8 (800) 123-45-67
+                    </a>
+                </div>
+                <div class="header__item1__mail header__elem">
+                    <a href="#" class="header__item1__mail__a">
+                        sales@log.ru
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="header__item2 flex-group_sp-btvn">
+            <div class="header__item2__elem1 flex-group_sp-btvn">
+                <div class="header__item2__logo flex-group_sp-btvn">
+                    <div class="logo__imag">
+                        <a href="#">
+                            <img src="imag/logo.png" alt="">
+                        </a>
+                    </div>
+                    <div class="logo__text">
+                        <a href="#" class="logo__text_a">
+                            <p class="logo__text_elem1">
+                                company
+                            </p>
+                            <p class="logo__text_elem2">
+                                tagline goes here
+                            </p>
+                        </a>
+                    </div>
+                </div>
+                <div class="header__item2__orientation">
+                    <a href="#" class="orientation__a">
+                        <div class="orientation__icon"></div>
+                        <span class="orientation_text _style-text-m">
+                            Наши офисы
+                        </span>
+                    </a>
+                </div>
+            </div>
+            <div class="header__item2__elem2">
+                <ul class="header__menu">
+                    <li class="header__menu__li _style-text-m"><a href="#">услуги</a></li>
+                    <li class="header__menu__li _style-text-m"><a href="#">О компании</a></li>
+                    <li class="header__menu__li _style-text-m"><a href="#">вакансии</a></li>
+                    <li class="header__menu__li _style-text-m"><a href="#">контакты</a></li>
+                </ul>
+            </div>
+        </div>
+    </section>
+    <section class="presentation flex-group_sp-btvn">
+        <div class="presentation__info flex-group_column">
+            <h1 class="presentation__info__elem1">Грузовые перевозки</h1>
+            <span class="presentation__info__elem2 _style-text-l">
+                Перевозка крупногабаритных грузов по России и Европе.
+                Полное экспедирование, страхование, упаковка и хранение грузов
+            </span>
+        </div>
+    </section>
+    <section id="services">
+        <div class="fon2 fon2_left"></div>
+        <div class="fon2 fon2_right"></div>
+        <div class="section-size flex-group_sp-btvn">
+            <div class="services_item1 flex-group_column">
+                <div class="services_item1__elem1"></div>
+                <div class="services_item1__elem2">
+                    грузоперевозки<br>
+                    по европе
+                </div>
+                <div class="services_item1__elem3 _style-text-l ">
+                    Страны Евросоюза, а также Украина,<br>
+                    Белоруссия со всеми документами.
+                </div>
+                <a href="#" class="services_item1__a _style-text-bott ">
+                    <div class="services_item1__elem4">ПОДРОБНЕЕ</div>
+                </a>
+            </div>
+            <div class="services_item2">
+                <img src="imag/truck.png" alt="">
+            </div>
+            <div class="services_item3 flex-group_column">
+                <div class="services_item3__next flex-group_sp-btvn">
+                    <span class="services__direction _style-text-m"><a href="#">след</a></span>
+                    <a href="#"><img class="services__arrows" src="imag/next.png" alt=""></a>
+                </div>
+                <div class="services_item3__back flex-group_sp-btvn">
+
+                    <a href="#"><img class="services__arrows" src="imag/back.png" alt=""></a>
+                    <span class="services__direction _style-text-m"><a href="#">Назад</a></span>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="provide" class="section-size">
+        <div class="section-top flex-group_column">
+            <div class="services_item1__elem1"></div>
+            <div class="services_item1__elem2">
+                услуги
+            </div>
+        </div>
+        <div class="provide__main-box">
+            <div class="provide__stripe flex-group_sp-btvn">
+
+                <a href="#">
+                    <div class="provide__elem provide__elem__activ flex-group_column">
+                        <div class="provide__elem__activ__imag">
+                            <img src="imag/Vector(1).png" alt="">
+                        </div>
+                        <span class="_style-text-l">Авиаперевозки</span>
+                    </div>
+                </a>
+
+                <a href="#">
+                    <div class="provide__elem flex-group_column">
+                        <img src="imag/Vector.png" alt="">
+                        <span class="_style-text-l">Авиаперевозки</span>
+                    </div>
+                </a>
+                <a href="#">
+                    <div class="provide__elem flex-group_column">
+                        <img src="imag/Vector.png" alt="">
+                        <span class="_style-text-l">Авиаперевозки</span>
+                    </div>
+                </a>
+                <a href="#">
+                    <div class="provide__elem flex-group_column">
+                        <img src="imag/Vector.png" alt="">
+                        <span class="_style-text-l">Авиаперевозки</span>
+                    </div>
+                </a>
+            </div>
+            <div class="provide__stripe flex-group_sp-btvn">
+                <a href="#">
+                    <div class="provide__elem flex-group_column">
+                    <img src="imag/Vector.png" alt="">
+                    <span class="_style-text-l">Авиаперевозки</span>
+                </div>
+                </a>
+                <a href="#">
+                    <div class="provide__elem flex-group_column">
+                        <img src="imag/Vector.png" alt="">
+                        <span class="_style-text-l">Авиаперевозки</span>
+                    </div>
+                </a>
+                <a href="#">
+                    <div class="provide__elem flex-group_column">
+                        <img src="imag/Vector.png" alt="">
+                        <span class="_style-text-l">Авиаперевозки</span>
+                    </div>
+                </a>
+                <a href="#">
+                    <div class="provide__elem flex-group_column">
+                        <img src="imag/Vector.png" alt="">
+                        <span class="_style-text-l">Авиаперевозки</span>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </section>
+</main>
+</body>
+</html>
